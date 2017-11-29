074f13da8ec7422cda68c6bacb4373697662d662
prasannalakshmiy <lakshmi.prasanna073@gmail.com>
2017-11-29 12:09:51 +0530

    style component, product-list.component.css

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,8 +1,10 @@
 import {Component} from '@angular/core';
+import {IProduct} from './product';
 
 @Component({
     selector:'pm-products',
-    templateUrl:'./product-list.component.html'
+    templateUrl:'./product-list.component.html',
+    styleUrls:['./Product-list.component.css']
 })
 
 export class ProductListComponent{
@@ -11,7 +13,8 @@ export class ProductListComponent{
     imageMargin : number = 2;
     showImage : boolean = false;
     listFilter : string = 'cart';
-    products: any[] = [
+
+    products: IProduct[] = [
     {
         "productId": 1,
         "productName": "Leaf Rake",
