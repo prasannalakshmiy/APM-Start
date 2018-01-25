32b9ee357bd2267bba4c2a010603823d689e5467
prasannalakshmiy <lakshmi.prasanna073@gmail.com>
2018-01-25 15:49:16 +0530

    routing : passing id

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 import {IProduct} from './product';
 
 @Component({
@@ -9,9 +11,21 @@ import {IProduct} from './product';
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product : IProduct;
-  constructor() { }
+  constructor(private _route : ActivatedRoute) { }
 
   ngOnInit() {
+    let id = +this._route.snapshot.paramMap.get('id');
+    this.pageTitle += `:${id}`; // o/p : Product Detail 10   ,    here ---- > 10 is the id
+    this.product = {
+      "productId":id,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2016",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    }
   }
 
 }
