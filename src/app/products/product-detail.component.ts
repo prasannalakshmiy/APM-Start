37cf26426d39c19c86c39d3451a436e5b2e84f2d
prasannalakshmiy <lakshmi.prasanna073@gmail.com>
2018-02-06 16:03:42 +0530

    productdetail page complete

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
-import {IProduct} from './product';
+import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'pm-product-detail',
@@ -11,7 +12,8 @@ import {IProduct} from './product';
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product : IProduct;
-  constructor(private _route : ActivatedRoute, private _router : Router) { 
+  errorMessage : string;
+  constructor(private _route : ActivatedRoute, private _router : Router, private _productService: ProductService) { 
 
   }
 
@@ -22,16 +24,23 @@ export class ProductDetailComponent implements OnInit {
   ngOnInit() {
     let id = +this._route.snapshot.paramMap.get('id');
     this.pageTitle += `:${id}`; // o/p : Product Detail 10   ,    here ---- > 10 is the id
-    this.product = {
-      "productId":id,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2016",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-    }
+    this.getSingleProduct(id);
+    // this.product = {
+    //   "productId":id,
+    //   "productName": "Leaf Rake",
+    //   "productCode": "GDN-0011",
+    //   "releaseDate": "March 19, 2016",
+    //   "description": "Leaf rake with 48-inch wooden handle.",
+    //   "price": 19.95,
+    //   "starRating": 3.2,
+    //   "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    // }
+  }
+
+  getSingleProduct(id : number){
+    this._productService.getSingleProduct(id).subscribe(
+      product => this.product = product,
+      error => this.errorMessage = <any>error);
   }
 
 }
